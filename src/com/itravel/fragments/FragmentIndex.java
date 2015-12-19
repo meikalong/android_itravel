19d9f4210d44fb81cd1ad80a22142804f9839df6
xiaolanglang <1712877559@qq.com>
2015-12-19 18:02:59 +0800

    加入拦截器功能，加载本地静态文件
    Signed-off-by: xiaolanglang <1712877559@qq.com>

--- src/com/itravel/fragments/FragmentIndex.java
+++ src/com/itravel/fragments/FragmentIndex.java
@@ -1,10 +1,12 @@
 package com.itravel.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,6 +22,7 @@ public class FragmentIndex extends Fragment {
 		return inflater.inflate(R.layout.fragment_index, container, false);
 	}
 
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public void onStart() {
 		super.onStart();
@@ -31,9 +34,31 @@ public class FragmentIndex extends Fragment {
 				view.loadUrl(url);
 				return super.shouldOverrideUrlLoading(view, url);
 			}
+
+			// 拦截请求，加载本地静态文件，例如加载本地js文件或者css文件，加快页面加载速度
+			// @Override
+			// public WebResourceResponse shouldInterceptRequest(WebView view,
+			// String url) {
+			// WebResourceResponse response = null;
+			// if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// response = super.shouldInterceptRequest(view, url);
+			// if (url.contains("icon.png")) {
+			// try {
+			// response = new WebResourceResponse("image/png",
+			// "UTF-8", getResources().getAssets().open(
+			// "icon.png"));
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// }
+			// }
+			// }
+			// return response;
+			// }
+
 		});
 
-		webView.getSettings().setJavaScriptEnabled(true);
-	}
+		WebSettings setting = webView.getSettings();
+		setting.setJavaScriptEnabled(true);
 
+	}
 }
